bc1a976c0c23cb104f4e9c001e70371f86f1065e
moonrill <arilramadani245@gmail.com>
2023-11-09 16:21:27 +0700

    fix : error authentication message

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : abort(response()->json(['error' => 'Unauthorized'],401));
+        return $request->expectsJson() ? null : abort(response()->json(['error' => ['message' => ['Unauthorized']]],401));
     }
 }
